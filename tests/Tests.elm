17e9e12a8f3645cbb542c763d185b24d0f4e8ac1
Greg Ziegan <greg.ziegan@gmail.com>
2017-04-08 20:31:21 -0700

    test updateAnySelectedAnnotations

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -179,32 +179,37 @@ tspanSelector { start, end, fontSize, fill } =
     ]
 
 
+testColor : Color.Color
+testColor =
+    Color.blue
+
+
 all : Test
 all =
     describe "Annotation App Suite"
-        [ describe "Drawing"
-            [ test "finishLineDrawing should add a line annotation to the edit history" <|
+        [ describe "finishLineDrawing"
+            [ test "should add a line annotation to the edit history" <|
                 \() ->
                     model
                         |> finishLineDrawing start end StraightLine DrawingLine
                         |> getFirstAnnotation
                         |> Maybe.map (Expect.equal (Lines StraightLine <| Line start end model.strokeColor model.strokeStyle))
                         |> Maybe.withDefault (Expect.fail "Array missing line annotation")
-            , test "finishLineDrawing should add an arrow annotation to the edit history" <|
+            , test "should add an arrow annotation to the edit history" <|
                 \() ->
                     model
                         |> finishLineDrawing start end Arrow DrawingLine
                         |> getFirstAnnotation
                         |> Maybe.map (Expect.equal (Lines Arrow <| Line start end model.strokeColor model.strokeStyle))
                         |> Maybe.withDefault (Expect.fail "Array missing arrow annotation")
-            , test "finishShapeDrawing should add a shape annotation to the edit history" <|
+            , test "should add a shape annotation to the edit history" <|
                 \() ->
                     model
                         |> finishShapeDrawing start end Rect DrawingShape
                         |> getFirstAnnotation
                         |> Maybe.map (Expect.equal (Shapes Rect <| Shape start end model.fill model.strokeColor model.strokeStyle))
                         |> Maybe.withDefault (Expect.fail "Array missing rect annotation")
-            , test "finishShapeDrawing should add a spotlight annotation with a spotlight fill to the edit history" <|
+            , test "should add a spotlight annotation with a spotlight fill to the edit history" <|
                 \() ->
                     model
                         |> finishSpotlightDrawing start end Rect DrawingShape
@@ -212,14 +217,38 @@ all =
                         |> Maybe.map (Expect.equal (Spotlight Rect <| Shape start end SpotlightFill model.strokeColor model.strokeStyle))
                         |> Maybe.withDefault (Expect.fail "Array missing spotlight rect annotation")
             ]
-          -- , describe "updating annotation attributes"
-          --     [ test "annotation update function is called on selected annotation" <|
-          --        \() ->
-          --
-          --           -- { model | annotationState = SelectedAnnotation 0 }
-          --           --   |> updateAnySelectedAnnotations updateAnnotationFn
-          --           --   |> Array.get
-          --     ]
+        , describe "updateAnySelectedAnnotations"
+            [ test "updates a Selected annotation" <|
+                \() ->
+                    model
+                        |> addAnnotation (Lines Arrow aLine)
+                        |> selectAnnotation 0
+                        |> updateAnySelectedAnnotations (updateStrokeColor testColor)
+                        |> getFirstAnnotation
+                        |> Maybe.map (Expect.equal (Lines Arrow <| Line start end testColor model.strokeStyle))
+                        |> Maybe.withDefault (Expect.fail "Array missing desired annotation")
+            , test "does not update a NotSelected annotation" <|
+                \() ->
+                    model
+                        |> addAnnotation (Lines Arrow aLine)
+                        |> updateAnySelectedAnnotations (updateStrokeStyle DashedMedium)
+                        |> getFirstAnnotation
+                        |> Maybe.map (Expect.equal (Lines Arrow aLine))
+                        |> Maybe.withDefault (Expect.fail "Array missing desired annotation")
+            , test "only updates the Selected annotation" <|
+                \() ->
+                    model
+                        |> addAnnotation (Lines Arrow aLine)
+                        |> addAnnotation (Shapes Rect aShape)
+                        |> addAnnotation (Shapes Ellipse aShape)
+                        |> selectAnnotation 1
+                        |> updateAnySelectedAnnotations (updateFill (SolidFill testColor))
+                        |> .edits
+                        |> .present
+                        |> Array.get 2
+                        |> Maybe.map (Expect.equal (Shapes Ellipse aShape))
+                        |> Maybe.withDefault (Expect.fail "Array missing desired annotation")
+            ]
         , describe "annotations"
             [ test "A straight line has the appropriate view attributes" <|
                 \() ->
