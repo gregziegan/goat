6350d44f6f9af7ff942c10314485c54e790e6256
Greg Ziegan <greg.ziegan@gmail.com>
2017-04-27 00:24:09 -0700

    Narrowing arrow (#68)
    * narrowing arrow rect path
    
    * lolz
    
    * it works!!!
    
    * arrow head attached!
    
    * minor tweak
    
    * ugly fix for moving arrow head with annotation
    
    * move vertices into correct position
    
    * finish the arrow!
    
    * test the view attributes of the new arrow

--- src/Goat/View/Utils.elm
+++ src/Goat/View/Utils.elm
@@ -5,6 +5,7 @@ import Html exposing (Attribute)
 import Html.Events exposing (on)
 import Json.Decode as Json
 import Rocket exposing ((=>))
+import Goat.Utils exposing (arrowAngle)
 
 
 onMouseDown : Json.Decoder msg -> Attribute msg
@@ -113,3 +114,103 @@ annotationStateToCursor annotationState =
 
         _ ->
             "crosshair"
+
+
+arrowHeadPath : EndPosition -> String
+arrowHeadPath pos =
+    "M"
+        ++ toString (toFloat pos.x - 20.6667)
+        ++ ","
+        ++ toString (toFloat pos.y - 2.8)
+        ++ "l-4.62033,-10.72559l 25.66667, 13.66667l -25.66667, 13.66667l4.62033, -10.33667z"
+
+
+arrowPath : Shape -> String
+arrowPath shape =
+    let
+        theta =
+            (2 * pi)
+                - (arrowAngle shape.start shape.end)
+
+        perpen =
+            (pi / 2) - theta
+
+        comp =
+            pi - theta
+
+        start =
+            { x = toFloat shape.start.x, y = toFloat shape.start.y }
+
+        end =
+            { x =
+                if shape.end.x > shape.start.x && shape.end.y > shape.start.y then
+                    (-20 * cos -theta) + toFloat shape.end.x
+                else if shape.end.x < shape.start.x && shape.end.y < shape.start.y then
+                    (20 * cos -comp) + toFloat shape.end.x
+                else if shape.end.x < shape.start.x && shape.end.y > shape.start.y then
+                    (20 * cos -comp) + toFloat shape.end.x
+                else
+                    (-20 * cos -theta) + toFloat shape.end.x
+            , y =
+                if shape.end.x > shape.start.x && shape.end.y > shape.start.y then
+                    (-20 * sin -theta) + toFloat shape.end.y
+                else if shape.end.x < shape.start.x && shape.end.y < shape.start.y then
+                    (-20 * sin -comp) + toFloat shape.end.y
+                else if shape.end.x < shape.start.x && shape.end.y > shape.start.y then
+                    (-20 * sin -comp) + toFloat shape.end.y
+                else
+                    (20 * sin theta) + toFloat shape.end.y
+            }
+
+        dx =
+            end.x - start.x
+
+        dy =
+            end.y - start.y
+
+        halfWayPt =
+            dx * 0.54286
+
+        arcPt =
+            dx * 0.85714
+    in
+        "M "
+            ++ toString start.x
+            ++ ","
+            ++ toString start.y
+            ++ "l"
+            ++ toString (4 * cos perpen)
+            ++ ","
+            ++ toString (4 * sin perpen)
+            ++ "c"
+            ++ toString halfWayPt
+            ++ ","
+            ++ toString (dy * 0.54286)
+            ++ " "
+            ++ toString arcPt
+            ++ ","
+            ++ toString (dy * 0.85714)
+            ++ " "
+            ++ toString (dx + (2.5 * cos perpen))
+            ++ ","
+            ++ toString (dy + (2.5 * sin perpen))
+            ++ "l "
+            ++ toString (-13 * cos (-theta + (pi / 2)))
+            ++ ","
+            ++ toString (-13 * sin (-theta + (pi / 2)))
+            ++ "c"
+            ++ toString (arcPt - dx + (2.5 * cos perpen))
+            ++ ","
+            ++ toString (((dy * 0.85714) - dy) + (2.5 * sin perpen))
+            ++ " "
+            ++ toString (halfWayPt - dx + (2.5 * cos perpen))
+            ++ ","
+            ++ toString (((dy * 0.54286) - dy) + (2.5 * sin perpen))
+            ++ " "
+            ++ toString (-dx + (2.5 * cos perpen))
+            ++ ","
+            ++ toString (-dy + (2.5 * sin perpen))
+            ++ "l"
+            ++ toString (4 * cos perpen)
+            ++ ","
+            ++ toString (4 * sin perpen)
