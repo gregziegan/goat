962753afb8ce5433fe7f616a73c12b83fc5392e4
Greg Ziegan <greg.ziegan@gmail.com>
2017-05-09 23:49:24 -0700

    remove commented out tests

--- tests/src/Tests.elm
+++ tests/src/Tests.elm
@@ -2,14 +2,6 @@ module Tests exposing (..)
 
 import Update
 import Update.Controls
-
-
--- import Update.DrawingAnnotation
--- import Update.EditingATextBox
--- import Update.MovingAnnotation
--- import Update.ResizingAnnotation
--- import Annotation
-
 import Utils
 import Test exposing (..)
 import View.DrawingArea.Annotation
@@ -21,11 +13,7 @@ all =
     describe "Annotation App Suite"
         [ Update.Controls.all
 
-        -- , Update.DrawingAnnotation.all
         -- , Annotation.all
-        -- , Update.MovingAnnotation.all
-        -- , Update.ResizingAnnotation.all
-        -- , Update.EditingATextBox.all
         , View.DrawingArea.Annotation.all
         , View.DrawingArea.Definitions.all
         , Update.all
