81c778f39d47b8812dea2d4077ec02fee4391686
Greg Ziegan <greg.ziegan@gmail.com>
2017-05-09 23:32:37 -0700

    bring back control ui tests

--- tests/src/Tests.elm
+++ tests/src/Tests.elm
@@ -1,9 +1,9 @@
 module Tests exposing (..)
 
 import Update
+import Update.Controls
 
 
--- import Update.Controls
 -- import Update.DrawingAnnotation
 -- import Update.EditingATextBox
 -- import Update.MovingAnnotation
@@ -19,13 +19,14 @@ import View.DrawingArea.Definitions
 all : Test
 all =
     describe "Annotation App Suite"
-        -- [ Update.Controls.all
-        [ -- , Update.DrawingAnnotation.all
-          -- , Annotation.all
-          -- , Update.MovingAnnotation.all
-          -- , Update.ResizingAnnotation.all
-          -- , Update.EditingATextBox.all
-          View.DrawingArea.Annotation.all
+        [ Update.Controls.all
+
+        -- , Update.DrawingAnnotation.all
+        -- , Annotation.all
+        -- , Update.MovingAnnotation.all
+        -- , Update.ResizingAnnotation.all
+        -- , Update.EditingATextBox.all
+        , View.DrawingArea.Annotation.all
         , View.DrawingArea.Definitions.all
         , Update.all
         , Utils.all
