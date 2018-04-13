73d206c8741b9c5bb32157baf6ddbd3e60545160
Greg Ziegan <greg.ziegan@gmail.com>
2018-04-13 17:05:00 -0500

    revert inline image fix

--- docs/index.js
+++ docs/index.js
@@ -126,7 +126,7 @@ function loadImage(src) {
 }
 
 function toImageObject(image) {
-  var id = image.getAttribute('data-image-uuid');
+  var id = image.getAttribute('data-imageuploadid');
   var originalWidth = parseFloat(image.getAttribute('data-original-width'));
   var originalHeight = parseFloat(image.getAttribute('data-original-height'));
   var width = parseFloat(image.style.width.replace('px', ''));
@@ -195,7 +195,7 @@ function exportToZendesk(imageUrl) {
     div.innerHTML = comment;
     var selectionBox = div.querySelector('span.zd-editor--rich-text-comment--resize-box');
 
-    var imageToReplace = div.querySelector('img[data-image-uuid="' + zendeskExportId + '"]');
+    var imageToReplace = div.querySelector('img[data-imageuploadid="' + zendeskExportId + '"]');
     if (selectionBox && imageToReplace) {
       selectionBox.outerHTML = '';
     } else {
